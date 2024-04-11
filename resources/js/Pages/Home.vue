c392a54838c5cec606a351911c5a2422b9d5e682
jakeb-k <jknow30@outlook.com>
2024-04-11 15:41:54 +1000

    budget model + mig

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Home" />
+
+    <GuestLayout>
+        <div class="h-dvh w-full bg-gray-700">
+            <p>Home Page</p>
+        </div>
+         
+    </GuestLayout>
+</template>
