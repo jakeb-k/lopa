1c7f428e1adef46d73a56c4913461118ed336dd4
jakeb-k <jknow30@outlook.com>
2024-04-13 14:18:01 +1000

    budget checker

--- resources/js/Components/Finance/Budget.vue
+++ resources/js/Components/Finance/Budget.vue
@@ -1,12 +1,16 @@
 <script setup>
-import {onMounted, ref} from 'vue'; 
+import {onMounted, ref, computed} from 'vue'; 
 
 import BudgetInfo from '@/Components/Finance/BudgetInfo.vue'; 
 import { Chart, registerables } from 'chart.js';
 
 var pieData = []; 
 var pieLabels = [];
+var total = 0;
+var overBudget = ref(false); 
+var income; 
 let myChart = null;  
+
 Chart.register(...registerables);
 
 
@@ -40,6 +44,20 @@ function createPieChart(pieData, pieLabels){
       }
     });
 }
+function budgetChecker(){
+    Object.keys(props.budgets).forEach(key => {
+       
+       if(props.budgets[key].name == 'Income') {
+            income = props.budgets[key].amount; 
+       }
+       else {
+            total += props.budgets[key].amount; 
+       }
+   });
+   if(total > income) {
+        overBudget = true; 
+   }
+}
 
 onMounted(() => {
     Object.keys(props.budgets).forEach(key => {
@@ -49,20 +67,30 @@ onMounted(() => {
             pieLabels.push(props.budgets[key].name);
         }
     });
-    console.log(pieData); 
+    budgetChecker(); 
     createPieChart(pieData, pieLabels); 
 }); 
+const budgetMsgStyle= computed(() =>
+    overBudget.value
+        ? 'text-red-400 text-lg font-bold w-full'
+        : 'text-green-600 text-lg font-bold basis-full'
+);
+const budgetMsg= computed(() =>
+    overBudget.value
+        ? 'You are over budget! Adjust your spendings!'
+        : 'Great work! You are within budget.'
+);
 </script>
 <template>
 
-    <div class="mt-10 p-8 mx-auto h-fit w-fit lg:w-3/5 lg:h-[450px] bg-gray-200 rounded-xl flex flex-row">
+    <div class="mt-10 p-8 mx-auto h-fit w-fit bg-gray-200 rounded-xl flex flex-row">
 
         <div class="flex flex-col w-1/2 mr-20">
         <h1 class="text-3xl underline text-gray-800 ">Budget Overview</h1>
           <div v-for="budget in props.budgets">
             <BudgetInfo :budget="budget"></BudgetInfo>
           </div>
-            
+          <span :class="budgetMsgStyle">{{ budgetMsg }}</span>
         </div>
         
         <div class="w-3/5">
