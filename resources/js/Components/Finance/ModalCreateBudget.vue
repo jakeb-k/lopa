0fcbd37e6ddf02802d31ee7c95429d3413f67df5
jakeb-k <jknow30@outlook.com>
2024-07-05 13:22:46 +1000

    [UI] set modal color for background

--- resources/js/Components/Finance/ModalCreateBudget.vue
+++ resources/js/Components/Finance/ModalCreateBudget.vue
@@ -1,96 +1,105 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue'; 
-import { router } from '@inertiajs/vue3';
-import { Inertia } from '@inertiajs/inertia';
-import { VueFinalModal } from 'vue-final-modal'
-import LoadingWheel from '../LoadingWheel.vue';
+import { ref, reactive } from "vue";
+import { router } from "@inertiajs/vue3";
+import { Inertia } from "@inertiajs/inertia";
+import { VueFinalModal } from "vue-final-modal";
+import LoadingWheel from "../LoadingWheel.vue";
 
 const props = defineProps<{
-  isSubBudget: boolean,
-  budgetId: number,
-}>()
+    isSubBudget: boolean;
+    budgetId: number;
+}>();
 
-const isSubBudget = ref(props.isSubBudget); 
+const isSubBudget = ref(props.isSubBudget);
 
 const budget = reactive({
-  name: '', 
-  amount: 0,
-  progress: 0,
-  isPaid: false, 
-  budgetId: props.budgetId,
+    name: "",
+    amount: 0,
+    progress: 0,
+    isPaid: false,
+    budgetId: props.budgetId,
 });
 
 const emit = defineEmits<{
-  (e: 'confirm'): void
-}>()
+    (e: "confirm"): void;
+}>();
 
 var isSubmitting = ref(false);
 
 function createBudget() {
-  if(isSubBudget){
-    isSubmitting.value = true;  // Start submission
-    // Use Inertia.put to make a PUT request
-    Inertia.post(`public/subbudget`, budget, {
-      preserveScroll: true,
-      onSuccess: () => {
-        emit('confirm');
-        isSubmitting.value = false;  // End submission after reload completes
-      },
-      onError: () => {
-        console.error("Error during the update process");
-        isSubmitting.value = false;  // Reset on error as well
-      }
-    });
-  }
-  else {
-    isSubmitting.value = true;  // Start submission
+    if (isSubBudget) {
+        isSubmitting.value = true; // Start submission
+        // Use Inertia.put to make a PUT request
+        Inertia.post(`public/subbudget`, budget, {
+            preserveScroll: true,
+            onSuccess: () => {
+                emit("confirm");
+                isSubmitting.value = false; // End submission after reload completes
+            },
+            onError: () => {
+                console.error("Error during the update process");
+                isSubmitting.value = false; // Reset on error as well
+            },
+        });
+    } else {
+        isSubmitting.value = true; // Start submission
 
-    // Use Inertia.put to make a PUT request
-    Inertia.post(`public/budget`, budget, {
-      preserveScroll: true,
-      onSuccess: () => {
-        emit('confirm');
-        isSubmitting.value = false;  // End submission after reload completes
-      },
-      onError: () => {
-        console.error("Error during the update process");
-        isSubmitting.value = false;  // Reset on error as well
-      }
-    });
-  }
+        // Use Inertia.put to make a PUT request
+        Inertia.post(`public/budget`, budget, {
+            preserveScroll: true,
+            onSuccess: () => {
+                emit("confirm");
+                isSubmitting.value = false; // End submission after reload completes
+            },
+            onError: () => {
+                console.error("Error during the update process");
+                isSubmitting.value = false; // Reset on error as well
+            },
+        });
+    }
 }
 </script>
 
 <template>
-  <VueFinalModal
-    class="flex justify-center items-center"
-    content-class="flex flex-col lg:w-1/4 w-1/2 mx-4 p-4 bg-white dark:bg-gray-900 border dark:border-gray-700 rounded-lg space-y-2"
-  >
-  <LoadingWheel :isSubmitting=isSubmitting></LoadingWheel>
+    <VueFinalModal
+        class="flex justify-center items-center"
+        content-class="flex flex-col lg:w-1/4 w-1/2 mx-4 p-4 bg-white dark:bg-white border dark:border-gray-700 rounded-lg space-y-2"
+    >
+        <LoadingWheel :isSubmitting="isSubmitting"></LoadingWheel>
 
-    <h1 class="text-xl"  v-if="isSubBudget"> New Sub Budget </h1>
-    <h1 class="text-xl"  v-if="!isSubBudget"> New Budget </h1>
+        <h1 class="text-xl" v-if="isSubBudget">New Sub Budget</h1>
+        <h1 class="text-xl" v-if="!isSubBudget">New Budget</h1>
 
-    <label for="name" v-if="!isSubBudget">Name for New Budget</label>
-    <label for="name" v-if="isSubBudget">Name for New Sub Budget</label>
-    <input id="name" v-model="budget.name"> 
+        <label for="name" v-if="!isSubBudget">Name for New Budget</label>
+        <label for="name" v-if="isSubBudget">Name for New Sub Budget</label>
+        <input id="name" v-model="budget.name" />
 
-    <label for="amount" v-if="!isSubBudget">Amount for New Budget</label>
-    <label for="amount" v-if="isSubBudget">Amount for New Sub Budget</label>
-    <input id="amount" type="number" v-model="budget.amount"> 
+        <label for="amount" v-if="!isSubBudget">Amount for New Budget</label>
+        <label for="amount" v-if="isSubBudget">Amount for New Sub Budget</label>
+        <input id="amount" type="number" v-model="budget.amount" />
 
-    <label for="progress" v-if="!isSubBudget">Progress for New Budget</label>
-    <input id="progress" type="number" v-if="!isSubBudget" v-model="budget.progress"> 
+        <label for="progress" v-if="!isSubBudget"
+            >Progress for New Budget</label
+        >
+        <input
+            id="progress"
+            type="number"
+            v-if="!isSubBudget"
+            v-model="budget.progress"
+        />
 
-    <div v-if="isSubBudget" class="mt-8">
-      <label>
-      <input type="checkbox" v-model="budget.isPaid" />
-      Is Paid?
-      </label>
-    </div>
-    
-    <button class="mt-1 ml-auto px-2 border rounded-lg" @click="createBudget()">
-      CREATE
-    </button>
-  </VueFinalModal>
+        <div v-if="isSubBudget" class="mt-8">
+            <label>
+                <input type="checkbox" v-model="budget.isPaid" />
+                Is Paid?
+            </label>
+        </div>
+
+        <button
+            class="mt-1 ml-auto px-2 border rounded-lg"
+            @click="createBudget()"
+        >
+            CREATE
+        </button>
+    </VueFinalModal>
 </template>
--- resources/js/Components/Finance/ModalUpdateBudget.vue
+++ resources/js/Components/Finance/ModalUpdateBudget.vue
@@ -1,98 +1,103 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue'; 
-import { router } from '@inertiajs/vue3';
-import { Inertia } from '@inertiajs/inertia';
-import { VueFinalModal } from 'vue-final-modal'
-import LoadingWheel from '../LoadingWheel.vue';
+import { ref, reactive } from "vue";
+import { router } from "@inertiajs/vue3";
+import { Inertia } from "@inertiajs/inertia";
+import { VueFinalModal } from "vue-final-modal";
+import LoadingWheel from "../LoadingWheel.vue";
 
 const props = defineProps<{
-  name?: string,
-  amount: number,
-  progress: number,
-  id: number,
-  isSubBudget: boolean
-}>()
+    name?: string;
+    amount: number;
+    progress: number;
+    id: number;
+    isSubBudget: boolean;
+}>();
 var amount = ref(props.amount);
 var progress = ref(props.progress);
-var name = ref(props.name); 
-const isSubBudget = ref(props.isSubBudget); 
+var name = ref(props.name);
+const isSubBudget = ref(props.isSubBudget);
 
 const budget = reactive({
-  name: name, 
-  amount: amount,
-  progress: progress,
-  isPaid: false,
+    name: name,
+    amount: amount,
+    progress: progress,
+    isPaid: false,
 });
 
 const emit = defineEmits<{
-  (e: 'confirm'): void
-}>()
+    (e: "confirm"): void;
+}>();
 
 var isSubmitting = ref(false);
 
 function update() {
-  isSubmitting.value = true;  // Start submission
-  if(isSubBudget){
-      // Use Inertia.put to make a PUT request
-    Inertia.put(`public/subbudget/${props.id}`, budget, {
-      preserveScroll: true,
-      onSuccess: () => {
-        emit('confirm');
-        isSubmitting.value = false;  // End submission after reload completes
-      },
-      onError: () => {
-        console.error("Error during the update process");
-        isSubmitting.value = false;  // Reset on error as well
-      }
-    });
-  } 
-  else {
-    // Use Inertia.put to make a PUT request
-    Inertia.put(`public/budget/${props.id}`, budget, {
-    preserveScroll: true,
-    onSuccess: () => {
-      emit('confirm');
-      isSubmitting.value = false;  // End submission after reload completes
-    },
-    onError: () => {
-      console.error("Error during the update process");
-      isSubmitting.value = false;  // Reset on error as well
+    isSubmitting.value = true; // Start submission
+    if (isSubBudget) {
+        // Use Inertia.put to make a PUT request
+        Inertia.put(`public/subbudget/${props.id}`, budget, {
+            preserveScroll: true,
+            onSuccess: () => {
+                emit("confirm");
+                isSubmitting.value = false; // End submission after reload completes
+            },
+            onError: () => {
+                console.error("Error during the update process");
+                isSubmitting.value = false; // Reset on error as well
+            },
+        });
+    } else {
+        // Use Inertia.put to make a PUT request
+        Inertia.put(`public/budget/${props.id}`, budget, {
+            preserveScroll: true,
+            onSuccess: () => {
+                emit("confirm");
+                isSubmitting.value = false; // End submission after reload completes
+            },
+            onError: () => {
+                console.error("Error during the update process");
+                isSubmitting.value = false; // Reset on error as well
+            },
+        });
     }
-  });
-  }
- 
 }
 </script>
 
 <template>
-  <VueFinalModal
-    class="flex justify-center items-center"
-    content-class="flex flex-col lg:w-1/4 w-1/2 mx-4 p-4 bg-white dark:bg-gray-900 border dark:border-gray-700 rounded-lg space-y-2"
-  >
-  <LoadingWheel :isSubmitting=isSubmitting></LoadingWheel>
+    <VueFinalModal
+        class="flex justify-center items-center"
+        content-class="flex flex-col lg:w-1/4 w-1/2 mx-4 p-4 bg-white dark:bg-white border dark:border-gray-700 rounded-lg space-y-2"
+    >
+        <LoadingWheel :isSubmitting="isSubmitting"></LoadingWheel>
 
-    <h1 class="text-xl">
-      {{ name }}
-    </h1>
-    
-    <label for="name">Edit Name: {{ name }}</label>
-    <input id="name" type="text" v-model="budget.name"> 
+        <h1 class="text-xl">
+            {{ name }}
+        </h1>
 
-    <label for="amount">Amount for {{ name }}</label>
-    <input id="amount" type="number" v-model="budget.amount"> 
+        <label for="name">Edit Name: {{ name }}</label>
+        <input id="name" type="text" v-model="budget.name" />
 
-    <label v-if="!isSubBudget" for="progress">Progress for {{ name }}</label>
-    <input v-if="!isSubBudget" id="progress" type="number" v-model="budget.progress"> 
+        <label for="amount">Amount for {{ name }}</label>
+        <input id="amount" type="number" v-model="budget.amount" />
 
-    <div v-if="isSubBudget" class="mt-8">
-      <label>
-      <input type="checkbox" v-model="budget.isPaid" />
-      Is Paid?
-      </label>
-    </div>
-    
-    <button class="mt-1 ml-auto px-2 border rounded-lg" @click="update()">
-      UPDATE
-    </button>
-  </VueFinalModal>
+        <label v-if="!isSubBudget" for="progress"
+            >Progress for {{ name }}</label
+        >
+        <input
+            v-if="!isSubBudget"
+            id="progress"
+            type="number"
+            v-model="budget.progress"
+        />
+
+        <div v-if="isSubBudget" class="mt-8">
+            <label>
+                <input type="checkbox" v-model="budget.isPaid" />
+                Is Paid?
+            </label>
+        </div>
+
+        <button class="mt-1 ml-auto px-2 border rounded-lg" @click="update()">
+            UPDATE
+        </button>
+    </VueFinalModal>
 </template>
